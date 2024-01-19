d23cffc098e7250b5083e21c98e0b96cd698bec8
dmmond <dmmond@ncsu.edu>
2024-01-19 11:40:26 -0500

    [Implementation] CountingSort implemented.

--- DataStructures/src/edu/ncsu/csc316/dsa/sorter/CountingSorter.java
+++ DataStructures/src/edu/ncsu/csc316/dsa/sorter/CountingSorter.java
@@ -11,9 +11,30 @@ import edu.ncsu.csc316.dsa.data.Identifiable;
 public class CountingSorter<E extends Identifiable> implements Sorter<E> {
     
     public void sort(E[] data) {
-        // TODO Your code here
-        // Since we constrained E to be Identifiable,
-        // we can now access the .getId() method of E objects
-        // from within this sort method
+        E min = data[0];
+        E max = data[0];
+        for(int i = 0; i < data.length - 1; i++) {
+        	if(data[i].getId() < min.getId()) {
+        		min = data[i];
+        	}
+        	if(data[i].getId() > max.getId()) {
+        		max = data[i];
+        	}
+        }
+        int k = max.getId() - min.getId()+1;
+        int[] B = new int[k];
+        for(int i = 0; i < data.length - 1; i++) {
+        	B[data[i].getId() - min.getId()] = B[data[i].getId() - min.getId()] + 1;
+        }
+        for(int i = 1; i < k - 1; i++) {
+        	B[i] = B[i-1] + B[i];
+        }
+        @SuppressWarnings("unchecked")
+        E[] F = (E[])(new Identifiable[data.length]);
+        for(int i = data.length - 1; i > 0; i++) {
+        	F[B[data[i].getId() - min.getId()] - 1] = data[i];
+        	B[data[i].getId() - min.getId()] = B[data[i].getId() - min.getId()] - 1;
+        }
+        data = F;
     }
 }
